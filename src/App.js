9c3c9007a636f420d115f69a02a5e94dbcec512c
Denis Berezin <nocab139@yandex.ru>
2020-05-07 22:25:04 +0300

    commit 7

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React today es ttttttttttttttttttttt
+          Learn
         </a>
       </header>
     </div>
